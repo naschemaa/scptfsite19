4142dccd302babef4ce04243cef720e96540a0ee
naschemaa <96695646+naschemaa@users.noreply.github.com>
2021-12-27 11:28:28 +0100

    Create script.lua

---
+++ GeneralScripts/BadgeAwarder/script.lua
@@ -0,0 +1,15 @@
+--PUT IN A PART WITH A Value who nammed "BadgeID". Put the id of the badge in "BadgeID".
+function OnTouch(part)
+	if (part.Parent:FindFirstChild("Humanoid") ~= nil) then
+		local p = game.Players:GetPlayerFromCharacter(part.Parent)
+		if (p ~= nil) then
+			
+			local b = game:GetService("BadgeService")
+			b:AwardBadge(p.userId, script.Parent.BadgeID.Value)
+		end
+	end
+end
+
+script.Parent.Touched:connect(OnTouch)
+
+--By naschemaa.
